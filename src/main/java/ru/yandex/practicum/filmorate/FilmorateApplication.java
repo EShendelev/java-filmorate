fc473b4dee2e094819747fb780d73c72b9eb7bcb
EShendelev <sendeleve@yandex.ru>
2023-04-23 01:53:30 +0300

    optimized imports

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,10 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.yandex.practicum.filmorate.model.Film;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class FilmorateApplication {
