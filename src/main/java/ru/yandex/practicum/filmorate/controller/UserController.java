16a74f44f4e67a627203fdcc905d0d913ae9e387
EShendelev <sendeleve@yandex.ru>
2023-04-23 02:04:51 +0300

    static

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -14,7 +14,7 @@ import java.util.Collection;
 @RequestMapping("/users")
 public class UserController {
 
-    private final static Logger log = LoggerFactory.getLogger(UserController.class);
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
     @Autowired
     public UserController(UserService userService) {
