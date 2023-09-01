8e841dea46ab03c7e0b49405636c7f52d06dc293
S-Gilliver <shtyrkov.dmitry@yandex.ru>
2023-09-01 22:21:09 +0300

    style

--- src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/FilmStorage.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.storage.interfaces;
 
-import org.springframework.web.bind.annotation.RequestParam;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
